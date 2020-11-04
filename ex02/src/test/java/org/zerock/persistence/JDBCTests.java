bb18334a8fe2eaeadb2c2b304b6b53d34cdeb279
YoungGi <dudrl4612@naver.com>
2020-11-04 12:18:36 +0900

    spring

---
+++ ex02/src/test/java/org/zerock/persistence/JDBCTests.java
@@ -0,0 +1,34 @@
+package org.zerock.persistence;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class JDBCTests {
+
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {			
+			e.printStackTrace();
+		}		
+	}
+	
+	@Test
+	public void testConnection() {
+		try {
+		   Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE"
+				   										,"chae", "1234");
+		   log.info(con);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
