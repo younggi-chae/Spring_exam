a314a39bee198c3c057df9803893bf6065bdbc4f
YoungGi <dudrl4612@naver.com>
2020-11-29 23:41:45 +0900

    spring

--- ex02/src/main/java/org/zerock/mapper/BoardAttachMapper.java
+++ ex02/src/main/java/org/zerock/mapper/BoardAttachMapper.java
@@ -7,13 +7,9 @@ import org.zerock.domain.BoardAttachVO;
 public interface BoardAttachMapper {
 
 	public void insert(BoardAttachVO vo);
-
 	public void delete(String uuid);
-
 	public List<BoardAttachVO> findByBno(Long bno);
-
 	public void deleteAll(Long bno);
-
 	public List<BoardAttachVO> getOldFiles();
 
 }
